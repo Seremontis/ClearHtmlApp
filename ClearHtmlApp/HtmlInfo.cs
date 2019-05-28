adabdb4166338c4ed1dbab574311f5a65cb23778
Karol Ścigała <karols96@interia.pl>
2019-05-28 19:55:47 +0200

    Dodaj pliki projektów.

---
+++ ClearHtmlApp/HtmlInfo.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ClearHtmlApp
+{
+    enum UserOption
+    {
+        Link,
+        Images
+    }
+
+    class HtmlInfo
+    {
+        private string htmlContent;
+        private string uri;
+        UserOption userOption;
+        string[] regexConditionalSearch = {
+                @"\<a.{1,}href.{1,}",
+                @"\<img.{1,}src.{1,}",
+                };
+        string[] regexConditionalClear;
+
+        public HtmlInfo(int select,string page)
+        {
+            userOption = (UserOption)select;
+            Regex regex = new Regex(@"^((http)|(https)).{1,}");
+            if (regex.IsMatch(page))
+            {
+                uri = page;
+            }
+            else
+            {
+                uri = "http://"+page;
+            }
+            PrepareView();
+        }
+
+        private void PrepareView()
+        {
+            using (WebClient webClient = new WebClient())
+            {
+                webClient.Proxy = GlobalProxySelection.GetEmptyWebProxy();
+                webClient.Headers[HttpRequestHeader.UserAgent] = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.121 Safari/535.2";
+                try
+                {
+                    htmlContent = webClient.DownloadString(uri);
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }              
+            }
+        }
+
+        public List<string> ResultOfPage()
+        {
+            Console.WriteLine("Proszę czekać...");
+            Console.WriteLine(".......");
+            var resultcheck = RegexCheck(htmlContent);
+            var listToReturn = UrlFilter(resultcheck);
+            return listToReturn;
+        }
+
+        private MatchCollection RegexCheck(string html)
+        {
+            Regex regex = new Regex(regexConditionalSearch[(int)userOption]);
+            MatchCollection matching = regex.Matches(html);
+            return matching;
+        }
+
+        private List<string> UrlFilter(MatchCollection matches)
+        {
+            List<string> textoutput = new List<string>();
+            if (matches.Count > 0)
+            {
+                for (int i = 0; i < matches.Count; i++)
+                {
+                    textoutput.Add(ClearString(matches[i].ToString()));
+                }
+            }            
+            else
+            {
+                textoutput.Add("Brak wyników");
+            }
+            return textoutput;
+        }
+
+        private string ClearString(string line)
+        {
+            string[] RegexToClearString = {
+                @"([\<]|[\<][\/])[A-Za-z]{1,}[\>]",
+                @"[\<](img).(src)[\=].",
+                @".{2}(alt).{1,}[\/][\>]" };
+            foreach (var item in RegexToClearString)
+            {
+                line = Regex.Replace(line, item, "");
+            }
+            return line;
+        }
+
+    }
+}
---
+++ ClearHtmlApp/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace ClearHtmlApp
+{
+    class Program
+    {
+        private static int select = 0;
+        private static string[] answer = { "Odnośniki do strony", "Linki do obrazków ze strony" };
+        static string uri;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj. Podaj stronę HTML do przefiltrowania");
+            uri=Console.ReadLine();
+            ChooseOption();    
+        }
+
+        private static void ChooseOption()
+        {           
+            SetOption();
+            ClickHandler(Console.ReadKey());
+        }
+
+        private static void SetOption()
+        {
+            Console.Clear();
+            Console.WriteLine("Witaj. Podaj stronę HTML do przefiltrowania");
+            Console.WriteLine(uri);
+            Console.WriteLine("Wybierz opcję:");
+
+            for (int i = 0; i < answer.Length; i++)
+            {
+                if (i == select)
+                {
+                    Console.BackgroundColor = ConsoleColor.White;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine(i + ") " + answer[i]);
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    Console.WriteLine(i + ") " + answer[i]);
+                }
+            }
+        }
+
+        private static void ClickHandler(ConsoleKeyInfo keyInfo)
+        {
+            switch (keyInfo.Key)
+            {
+                case ConsoleKey.UpArrow:
+                    --select;
+                    CkeckOverflow();
+                    ChooseOption();
+                    break;
+                case ConsoleKey.DownArrow:
+                    ++select;
+                    CkeckOverflow();
+                    ChooseOption();
+                    break;
+                case ConsoleKey.Enter:
+                    SearchInPage();
+                    break;
+                default:
+                    break;
+            }           
+        }
+
+        private static void CkeckOverflow()
+        {
+            if (select < 0)
+            {
+                select = answer.Length - 1;
+            }
+            else if (select > answer.Length - 1)
+            {
+                select = 0;
+            }
+        }
+
+        private static void SearchInPage()
+        {
+            HtmlInfo html = new HtmlInfo(select,uri);
+            foreach (var item in html.ResultOfPage())
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }      
+    }
+}
